427354711f4c4cd60c59c26f2385422beeacc8fb
ali Rashad almanea <37377385+Alialmanea@users.noreply.github.com>
2020-09-21 19:23:28 +0300

    Create Buble Sort.py

---
+++ Buble Sort.py
@@ -0,0 +1,23 @@
+from random import randint
+
+
+
+def Bublesort(arr):
+  for i in range(len(arr)):
+    for j in range(len(arr)-i-1):
+      if arr[j+1] < arr[j]:
+        print(arr)
+        arr[j+1], arr[j] = arr[j], arr[j+1] 
+
+arr = []
+for _ in range(10):
+  arr.append(randint(-1, 10))
+print('Befor be Sorted Array : ',arr)
+Bublesort(arr)
+print(arr)
+
+
+def main():
+  pass
+
+main()  
